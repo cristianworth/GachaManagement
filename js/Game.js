fe7bedc06b20cdc2e5b96ab65287e5b7b2601b72
cristianworth <cristianweissmantel@gmail.com>
2025-03-14 21:19:01 -0300

    Refactor: changed database mathod to it's respective classes: Game and Task.

--- js/Game.js
+++ js/Game.js
@@ -22,3 +22,81 @@ allGames.push(new Game(id = 4, description = 'Wuthering Waves', abbreviation = '
 allGames.push(new Game(id = 5, description = 'Zenless Zone Zero', abbreviation = 'ZZZ', img = 'img/zzz-icon.png', capStamina = 240, staminaPerMinute = 6, currentStamina = 0, maxStaminaAt = '', dateMaxStamina = new Date(), pendingTasks = ''));
 // allGames.push(new Game(id = 6, description = 'Nikke', abbreviation = 'NKK', img = 'img/nikke-icon.png', capStamina = 1, staminaPerMinute = 1440, currentStamina = 0, maxStaminaAt = '', dateMaxStamina = new Date(), pendingTasks = ''));
 allGames.push(new Game(id = 7, description = 'Snowbreak', abbreviation = 'SK', img = 'img/snowbreak-icon.png', capStamina = 240, staminaPerMinute = 6, currentStamina = 0, maxStaminaAt = '', dateMaxStamina = new Date(), pendingTasks = ''));
+
+async function addGameIfNotExists(newGame) {
+    // Method used to populate the initial set o data predefined on the Game.js file
+    try {
+        const gameFound = await fetchGameById(newGame.id);
+
+        if (!gameFound) {
+            await addGame(newGame);
+        }
+    } catch (error) {
+        console.error("Failed to add game:", error);
+    }
+}
+
+async function addGame(game) {
+    try {
+        await db.games.add(game);
+        console.log("New Game added:", game);
+    } catch (error) {
+        console.error("Failed to add game:", error);
+    }
+}
+
+async function updateGame(game) {
+    // Method used to mainly update the amout of stamina on the main screen
+    if (!game.id) {
+        return;
+    }
+
+    try {
+        await db.games.update(game.id, game);
+    } catch (error) {
+        console.error("Erro ao atualizar o jogo:", error);
+    }
+}
+
+async function deleteGameById(gameId) {
+    // Not really used at the moment but can be used to update an existing Game, 
+    //      delet it and then when the pages reload the populateInitialData will create it again
+    // Examples:
+    // deleteGameById(2);
+    // deleteGameById(6);
+    // deleteGameById(7);
+    try {
+        const gameFound = await fetchGameById(gameId);
+
+        if (gameFound) {
+            await db.games.delete(gameId);
+        } else {
+            console.log(`Game with ID ${gameId} not found in the database.`);
+        }
+    } catch (error) {
+        console.error(`Failed to delete game with ID ${gameId}:`, error);
+    }
+}
+
+async function fetchAllGames() {
+    // Method used to load allGames into the main page. 
+    try {
+        const games = await db.games.orderBy("dateMaxStamina").toArray();
+        console.log("Todos os jogos:", games);
+        return games;
+    } catch (error) {
+        console.error("Erro ao buscar todos os jogos:", error);
+        return [];
+    }
+}
+
+async function fetchGameById(id) {
+    // Method to verify if the game exists
+    try {
+        const game = await db.games.get(id);
+        return game;
+    } catch (error) {
+        console.error("Erro ao buscar o jogo pelo ID:", error);
+        return null;
+    }
+}
--- js/Tasks.js
+++ js/Tasks.js
@@ -14,3 +14,97 @@ var allTasks = [];
 
 allTasks.push(new Tasks(id = 1, description = 'Spyral Abyss', expirationDate = '15/03/2025', isDone = 'S', refreshType = 'BiMonthly', gameId = 1, gameDescription = "Genshin Impact"));
 allTasks.push(new Tasks(id = 2, description = 'Imaginarium Theater', expirationDate = '31/03/2025', isDone = 'N', refreshType = 'BiMonthly', gameId = 1, gameDescription = "Genshin Impact"));
+
+async function addTaskIfNotExists(newTask) {
+    // Method used to populate the initial set o data predefined on the Tasks.js file
+    try {
+        const taskFound = await fetchTaskById(newTask.id);
+
+        if (!taskFound) {
+            await addTask(newTask);
+        }
+    } catch (error) {
+        console.error("Failed to add Task:", error);
+    }
+}
+
+async function addTask(task) {
+    try {
+        await db.tasks.add(task);
+        console.log("New Task added:", task);
+    } catch (error) {
+        console.error("Failed to add task:", error);
+    }
+}
+
+async function updateTask(task) {
+    if (!task.id) {
+        console.log("Invalid task object id: ", task);
+        return;
+    }
+
+    try {
+        await db.tasks.update(task.id, task);
+    } catch (error) {
+        console.error("Erro ao atualizar a tarefa:", error);
+    }
+}
+
+async function deleteTaskById(taskId) {
+    // Not really used at the moment but can be used to update an existing Game, 
+    //      delet it and then when the pages reload the populateInitialData will create it again
+    // Examples:
+    // deleteTaskById(1);
+    // deleteTaskById(2);
+    try {
+        const taskFound = await fetchTaskById(taskId);
+
+        if (taskFound) {
+            await db.tasks.delete(taskId);
+        } else {
+            console.log(`Task with ID ${taskId} not found in the database.`);
+        }
+    } catch (error) {
+        console.error(`Failed to delete task with ID ${taskId}:`, error);
+    }
+}
+
+async function fetchAllTasks() {
+    try {
+        const tasks = await db.tasks.orderBy("expirationDate").toArray();
+        console.log("Todas as tarefas:", tasks);
+        return tasks;
+    } catch (error) {
+        console.error("Erro ao buscar todas as tarefas:", error);
+        return [];
+    }
+}
+
+async function fetchTaskById(id) {
+    // Method to verify if the task exists
+    try {
+        const task = await db.tasks.get(id);
+        return task;
+    } catch (error) {
+        console.error("Erro ao buscar a tarefa pelo ID:", error);
+        return null;
+    }
+}
+
+async function fetchTasksByGame(gameId) {
+    try {
+        var task = await db.tasks.where("gameId").equals(gameId).toArray();
+        return task;
+    } catch (error) {
+        console.error("Failed to add game:", error);
+    }
+}
+
+async function completeTask(taskId) {
+    try {
+        var task = await db.tasks.update(taskId, { status: "completed" });
+        return task;
+    } catch (error) {
+        console.error("Failed to update game:", error);
+    }
+}
